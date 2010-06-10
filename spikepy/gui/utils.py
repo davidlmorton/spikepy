c8ac5ab1d0764c88b4a4bb6be67045349139baec
David Morton <davidlmorton@gmail.com>
2010-06-10 23:05:46 +0000

    added icons to gui section, and added FileListCtrl.

---
+++ spikepy/gui/utils.py
@@ -0,0 +1,18 @@
+"""
+A collection of utility functions for the spikepy gui.
+"""
+import os
+
+import wx
+
+gui_folder  = os.path.split(__file__)[0]
+icon_folder = os.path.join(gui_folder, 'icons')
+
+def get_bitmap_icon(name):
+    icon_files = os.listdir(icon_folder)
+    for file in icon_files:
+        if os.path.splitext(file)[0] == name:
+            image = wx.Image(os.path.join(icon_folder, file))
+            return image.ConvertToBitmap()
+    raise RuntimeError("Cannot find image named %s in icons folder." % name)
+    
