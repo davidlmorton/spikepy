bfa2d650781b2e101cd3847c7db42404e94506ce
David Morton <davidlmorton@gmail.com>
2010-06-11 20:10:53 +0000

    Added a status bar and menu bar.

--- spikepy/aui_docking_window.py
+++ spikepy/aui_docking_window.py
@@ -35,6 +35,38 @@ class MyFrame(wx.Frame):
 
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
+        # create status bar (I'm not sure if we'll use this or not)
+        self.CreateStatusBar()
+        self.SetStatusText("We can put status updates here if we want")
+        
+        # create menu bar
+        menubar = wx.MenuBar()
+        file_menu = wx.Menu()
+        file_menu.Append(101, "Open")
+        file_menu.Append(102, "Exit")
+        menubar.Append(file_menu, "File")
+        
+        edit_menu = wx.Menu()
+        edit_menu.Append(201, "Preferences")
+        menubar.Append(edit_menu, "Edit")
+        
+        view_menu = wx.Menu()
+        workspaces_submenu = wx.Menu()
+        workspaces_submenu.Append(3011, "Default")
+        workspaces_submenu.AppendSeparator()
+        workspaces_submenu.Append(3012, "No saved custom workspaces")
+        view_menu.AppendMenu(301, "Workspaces", workspaces_submenu)
+        menubar.Append(view_menu, "View")
+        
+        help_menu = wx.Menu()
+        about_id = wx.NewId() # Do we want to reference ID's with names as done
+                              # here or try to come up with a systematic ID 
+                              # numbering scheme as above?
+        help_menu.Append(about_id, "About")
+        menubar.Append(help_menu, "Help")
+        
+        self.SetMenuBar(menubar)
+
     def OnClose(self, event):
         # deinitialize the frame manager
         self._mgr.UnInit()
