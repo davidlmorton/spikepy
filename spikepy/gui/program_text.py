5b9026d105fcdc7cd4420c5d139bc18282b4d12b
David Morton <davidlmorton@gmail.com>
2010-06-14 20:51:57 +0000

    forgot to add program_text.py

---
+++ spikepy/gui/program_text.py
@@ -0,0 +1,8 @@
+
+about_text = ("Spikepy is a python-based spike sorting "
+            "framework. It has been designed to be general enough to implement "
+            "many different spike sorting algorithms. Spikepy can be used by "
+            "electrophysiologists without any additional programming required. "
+            "Additionally, spikepy can be easily extended to include many more "
+            "algorithms, or to mix and match aspects of any existing "
+            "algorithms.")
