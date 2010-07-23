dc1f67199449392ef87bf6150805e7b5b6c0f298
David Morton <davidlmorton@gmail.com>
2010-07-23 04:34:30 +0000

    Moved splash screen code to before all imports except wx, because     this is where it was taking so long on startup.

--- spikepy/spikepy_gui.py
+++ spikepy/spikepy_gui.py
@@ -1,10 +1,15 @@
 import wx
-from spikepy.gui.controller import Controller
+
 
 if __name__ == '__main__':
     app = wx.App()
+    from spikepy.gui.utils import get_bitmap_icon 
+    splash_screen = wx.SplashScreen(get_bitmap_icon('spikepy_splash'), 
+            wx.SPLASH_CENTRE_ON_SCREEN|wx.SPLASH_NO_TIMEOUT, 8000, None)
+    from spikepy.gui.controller import Controller
 
     controller = Controller()
     controller.setup_subscriptions()
+    splash_screen.Destroy()
 
     app.MainLoop()
