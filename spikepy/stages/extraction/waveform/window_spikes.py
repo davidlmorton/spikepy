5689695b8dc56f6423f43598dccb0eba4719e485
David Morton <davidlmorton@gmail.com>
2010-08-05 19:48:35 +0000

    Fixed bug in waveform window_spikes.py where overlapping spikes were not being     excluded from the good_spikes list.

--- spikepy/stages/extraction/waveform/window_spikes.py
+++ spikepy/stages/extraction/waveform/window_spikes.py
@@ -69,14 +69,12 @@ def determine_excluded_spikes(signal, spike_index_list, window_size,
     determine which (if any) indexes will be excluded due to overlapping
     """
     print spike_index_list
-    good_spike_index_list      = []
-    excluded_spike_index_list  = []
-    truncated_spike_index_list = []
+    good_spike_index_set      = set()
+    excluded_spike_index_set  = set()
+    truncated_spike_index_set = set()
     for i in xrange(len(spike_index_list)):
         # assume spike is good unless it overlaps with edge of another 
         #   spike window
-        spike_bad = False
-        
         si = spike_index_list[i]
         
         bi = si - pre_i   # proposed begining index
@@ -84,17 +82,17 @@ def determine_excluded_spikes(signal, spike_index_list, window_size,
         # test for spike too close to begining or end of signal
         if bi > 0 and ei < len(signal)-1:
             # test for begining overlapping with end of last spike
-            if i > 0 and bi < spike_index_list[i-1] + post_i:
-                excluded_spike_index_list.append(si)
-            # test for end overlapping with beginning of next spike
-            elif (i < len(spike_index_list)-1 and 
-                    ei > spike_index_list[i+1] - pre_i):
-                excluded_spike_index_list.append(si)
-            # doesn't overlap and isn't too close to ends of signal
-            good_spike_index_list.append(si)
+            if i > 0 and bi < (spike_index_list[i-1] + post_i):
+                excluded_spike_index_set.add(si)
+                excluded_spike_index_set.add(si)
+            else:
+                # doesn't overlap and isn't too close to ends of signal
+                good_spike_index_set.add(si)
         else: 
             # spike too close to start of signal or end of signal 
-            truncated_spike_index_list.append(si)
+            truncated_spike_index_set.add(si)
 
-    return (good_spike_index_list, truncated_spike_index_list, 
-                excluded_spike_index_list)
+    gsil = sorted(list(good_spike_index_set))
+    esil = sorted(list(excluded_spike_index_set))
+    tsil = sorted(list(truncated_spike_index_set))
+    return (gsil, esil, tsil)
