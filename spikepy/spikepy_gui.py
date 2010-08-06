5e019ce798ee3071cb5406dcd1056857f7acb179
David Morton <davidlmorton@gmail.com>
2010-08-06 05:56:55 +0000

    Fixed the splashscreen issue, and posted answer to stackoverflow.

--- spikepy/spikepy_gui.py
+++ spikepy/spikepy_gui.py
@@ -1,18 +1,23 @@
 import wx
 
+class MySplashScreen(wx.SplashScreen):
+    def __init__(self, image=None, splash_style=None, timeout=None, 
+                       parent=None, **kwargs):
+        wx.SplashScreen.__init__(self, image, splash_style, timeout, 
+                                 parent, **kwargs)
 
 if __name__ == '__main__':
-    from spikepy.gui.utils import get_bitmap_icon 
+    def startup():
+        from spikepy.gui.controller import Controller
+        controller = Controller()
+        controller.setup_subscriptions()
+        splash_screen.Destroy()
+    from spikepy.gui.utils import get_bitmap_icon
+
     app = wx.App()
-
-    splash_image = get_bitmap_icon('spikepy_splash')
-    splash_screen = wx.SplashScreen(splash_image, 
-            wx.SPLASH_CENTRE_ON_SCREEN|wx.SPLASH_NO_TIMEOUT, 0, None)
-    wx.Yield()
-
-    from spikepy.gui.controller import Controller
-    controller = Controller()
-    controller.setup_subscriptions()
-    splash_screen.Destroy()
-
+    splash_screen = MySplashScreen(image=get_bitmap_icon('spikepy_splash'), 
+                                   splash_style=wx.SPLASH_CENTRE_ON_SCREEN, 
+                                   timeout=1000, 
+                                   parent=None)
+    wx.CallLater(200, startup)
     app.MainLoop()
