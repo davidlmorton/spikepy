2bde498cbe2cd6e32ed5b9739148ce8e7acc8433
David Morton <davidlmorton@gmail.com>
2010-12-23 02:03:01 +0000

    Wrote more unit-like tests for file_interpreters.     Found bug with sampling_freq calculation.

--- spikepy/builtins/file_interpreters/tests/test_wessel_labview_text.py
+++ spikepy/builtins/file_interpreters/tests/test_wessel_labview_text.py
@@ -1,12 +1,16 @@
 import unittest
+import os
 
 from spikepy.builtins.file_interpreters.wessel_labview_text import \
     Wessel_LabView_text
 from spikepy.common.trial import Trial
 
+base_directory = os.path.split(__file__)[0]
+sample_data_fullpath = os.path.join(base_directory, 'sample_data_file.tet')
+
 class ReadingSampleDataFile(unittest.TestCase):
     file_interpreter = Wessel_LabView_text()
-    trial_list = file_interpreter.read_data_file('sample_data_file.wpt')
+    trial_list = file_interpreter.read_data_file(sample_data_fullpath)
     trial = trial_list[0]
 
     def test_list_of_trial_objects(self):
@@ -14,11 +18,17 @@ class ReadingSampleDataFile(unittest.TestCase):
         for trial in self.trial_list:
             self.assertTrue(isinstance(trial, Trial))
 
-    def test_trial_properly_constructed(self):
+    def test_num_traces(self):
         self.assertTrue(len(self.trial.raw_traces)==1)
+
+    def test_len_of_raw_traces(self):
         self.assertTrue(len(self.trial.raw_traces[0])==9)
+
+    def test_sampling_freq(self):
         self.assertTrue(self.trial.sampling_freq==10000)
-        self.assertTrue(self.trial.fullpath=='sample_data_file.wpt')
+
+    def test_fullpath(self):
+        self.assertTrue(self.trial.fullpath==sample_data_fullpath)
 
 if __name__ == '__main__':
     unittest.main()
--- spikepy/builtins/file_interpreters/wessel_labview_text_tetrode.py
+++ spikepy/builtins/file_interpreters/wessel_labview_text_tetrode.py
@@ -9,14 +9,11 @@ class Wessel_LabView_text_tetrode(FileInterpreter):
         self.description = '''A plain text file containing one trial.  Data are organized in columns.  Columns: 0,1,2,3=data(mV) 4=pulse_1 5=pulse_2 6=time(ms).'''
 
     def read_data_file(self, fullpath):
-        print "reading in data file"
         data = numpy.loadtxt(fullpath)
         raw_traces = data.T[:4]
         times = data.T[-1]
         sampling_freq = int((len(times)-1)/
                             (times[-1]-times[0])*1000) # 1000 kHz->Hz
 
-        print "making trial objects"
         trials = [self.make_trial_object(sampling_freq, raw_traces, fullpath)]
-        print "made trial objects (%d)" % len(trials)
         return trials
